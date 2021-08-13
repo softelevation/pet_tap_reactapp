8d886ea3378788b3924fe4688f5f0d6aecb2ca38
Bharat Chhabra <bharat.chhabra339@gmail.com>
2021-08-13 12:33:01 +0530

    changes

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,6 +4,7 @@ import * as React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
 import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import Home from '../screens/home';
+// import Home from '../screens/home';
 
 const Stack = createNativeStackNavigator();
 
